25169761a50ffee6fca6433fe85c0e3b9453dacc
D7703_29 <d7703_29@d7703-29ui-imac.local>
2018-03-14 15:37:22 +0900

    label

--- changer/ViewController.swift
+++ changer/ViewController.swift
@@ -3,23 +3,35 @@
 //  changer
 //
 //  Created by D7703_29 on 2018. 3. 14..
-//  Copyright © 2018년 김영석. All rights reserved.
+//  Copyright © 2018년 . All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var flag = 0
+    @IBOutlet var lbltext: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+view.backgroundColor = UIColor.yellow
     }
-
+    
+    @IBAction func button(_ sender: Any) {
+        if flag == 0 {
+            view.backgroundColor = UIColor.yellow
+            flag = 1
+            lbltext.text = "yellow"
+        } else if flag == 1 {
+            view.backgroundColor = UIColor.green
+            flag = 0
+            lbltext.text = "green"
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
